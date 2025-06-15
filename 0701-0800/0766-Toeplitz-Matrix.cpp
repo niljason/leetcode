9f3bcb15c1acca136009ada394cafe4edcf84e57
Jason Lin <8333jlin@gmail.com>
2025-06-15 12:18:19 -0400

    0766

---
+++ 0701-0800/0766-Toeplitz-Matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (i == 0 || j == 0) { continue; }
+                else if (matrix[i][j] != matrix[i-1][j-1]) { return false; }
+            }
+        }
+        return true;
+    }
+};
