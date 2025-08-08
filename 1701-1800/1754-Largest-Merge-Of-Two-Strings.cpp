644c127dda99d3cd9b56d97a756be72c13f7b251
Jason Lin <8333jlin@gmail.com>
2025-08-07 21:35:58 -0400

    1754

---
+++ 1701-1800/1754-Largest-Merge-Of-Two-Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestMerge(string word1, string word2) {
+        string merge = "";
+        while (word1.size() > 0 && word2.size() > 0) {
+            if (word1 >= word2) {
+                merge += word1[0];
+                word1 = word1.substr(1);
+            }
+            else {
+                merge += word2[0];
+                word2 = word2.substr(1);
+            }
+        }
+        merge += (word1 + word2);
+        return merge;
+    }
+};
