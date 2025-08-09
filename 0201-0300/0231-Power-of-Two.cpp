e76346c35370db15df87bcb81461cd39ca70c8ff
Jason Lin <8333jlin@gmail.com>
2025-08-08 21:11:40 -0400

    0231: Update to one liner solution using bitwise operations

--- 0201-0300/0231-Power-of-Two.cpp
+++ 0201-0300/0231-Power-of-Two.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n==1) { return true; }
-        if (n % 2 == 1 || n <= 0) { return false; }
-        return isPowerOfTwo(n/2);
+        return n > 0 && (n & n - 1) == 0;
     }
 };
