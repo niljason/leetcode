f6f4f318420d7e3c08542bf83f90215afd7ccb49
Jason Lin <8333jlin@gmail.com>
2025-08-15 21:05:49 -0400

    2264

---
+++ 2201-2300/2264-Largest-3-Same-Digit-Number-in-String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string largest = "";
+        for (int i = 1; i < num.size()-1; i++) {
+            if (num[i] == num[i-1] && num[i] == num[i+1]) {
+                largest = max(largest, num.substr(i-1, 3));
+            }
+        }
+        return largest;
+    }
+};
