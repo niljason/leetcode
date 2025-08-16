2824d914957c67330a54c8018b7b3bf75be33493
Jason Lin <8333jlin@gmail.com>
2025-08-15 21:07:06 -0400

    2787

---
+++ 2701-2800/2787-Ways-to-Express-an-Integer-as-Sum-of-Powers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        long long MOD = 1e9 + 7;
+        vector<long long> dp(n+1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long long val = pow(i, x);
+            if (val > n) { break;}
+            for (int j = n; j >= val; j--) {
+                dp[j] = (dp[j] + dp[j-val]) % MOD;
+            }
+        }
+        return dp[n];
+    }
+};
